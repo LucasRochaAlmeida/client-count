249159950480a2dd6ec09e2fe7a4018c67792903
Lucas <lucasrochadealmeida@hotmail.com>
2020-10-07 22:21:25 -0300

    ajustando pois uni as contas

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,11 +2,7 @@ import {Cliente} from "./Cliente.js";
 
 export class ContaCorrente{
     static numeroDeContas = 0;
-    agencia;
-
-    _cliente;
-
-    _saldo = 0;
+ 
 /* # serve para deixar a variável privada assim ela não pode ser alterada diretamente.
     No exemplo acima vc pode add valor usando o método 'depositar', mas não usando contaCorrente.saldo = tal valor
     # ainda não é oficial então usa-se _ para mostrar que é privada */
@@ -27,6 +23,7 @@ export class ContaCorrente{
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas+=1;
     }
 
