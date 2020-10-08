2bbc6f690cdcca53f511bc459b05fbbc0b8bf202
Lucas <lucasrochadealmeida@hotmail.com>
2020-10-07 22:22:38 -0300

    excluindo conta2 e add import

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import{Cliente} from "./Cliente.js"
 import{ContaCorrente} from "./ContaCorrente.js"
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente('Ricardo', 11122233309);
-const cliente2 = new Cliente('Alice', 22233311108);
 
-const conta1 = new ContaCorrente(1001, cliente1);
-
-const conta2 = new ContaCorrente(102, cliente2);
+const conta1 = new Conta(0, cliente1, 1001);
 
 conta1.depositar(500);
+conta1.sacar(100);
 
-let valor = 200;
+const contaPoupanca = new Conta(50, cliente1, 1001);
 
-conta1.transferir(valor, conta2);
-
-console.log(ContaCorrente.numeroDeContas)
+console.log(contaPoupanca)
+console.log(conta1)
 //console.log(conta2.numeroDeContas)
