963c0840fd6adb5f0c206cf6ac5797efcce7210e
Lucas <lucasrochadealmeida@hotmail.com>
2020-10-20 22:04:17 -0300

    add variaveis privadas, exportando/importando, heranças...

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,46 +1,24 @@
-import {Cliente} from "./Cliente.js";
 
-export class ContaCorrente{
+import { Conta } from "./Conta.js";
+
+export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
+    
+    constructor(cliente, agencia){
+        super(0, cliente, agencia)
+        ContaCorrente.numeroDeContas+=1;
+    }
+    sacar(valor){  //sobreescrevendo o comportamento de sacar
+        let taxa = 1.1
+        return this._sacar(valor, taxa);
+    }
+}
+/* A palavra-chave 'static' define um método estático de uma classe. 
+    Métodos estáticos são chamados sem a instanciação da sua classe e não podem ser chamados quando a classe
+    é instanciada. Métodos estáticos são geralmente usados para criar funções de utilidades por uma aplicação. */
  
 /* # serve para deixar a variável privada assim ela não pode ser alterada diretamente.
     No exemplo acima vc pode add valor usando o método 'depositar', mas não usando contaCorrente.saldo = tal valor
     # ainda não é oficial então usa-se _ para mostrar que é privada */
 
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
-            this._cliente = novoValor;
-        }
-    }
-
-    get cliente(){
-        return this._cliente;
-    }
-
-    get saldo(){
-        this._saldo;
-    }
-    constructor(agencia, cliente){
-        this.agencia = agencia;
-        this.cliente = cliente;
-        this._saldo = 0;
-        ContaCorrente.numeroDeContas+=1;
-    }
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-        this._saldo += valor;
-    }
-    transferir(valor, conta){
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-    }
-}
+    
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import{Cliente} from "./Cliente.js"
 import{ContaCorrente} from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js";
-import {Conta} from "./Conta.js";
+
 
 const cliente1 = new Cliente('Ricardo', 11122233309);
 
-const conta1 = new Conta(0, cliente1, 1001);
+const conta1 = new ContaCorrente(cliente1, 1001);
 
 conta1.depositar(500);
 conta1.sacar(100);
 
-const contaPoupanca = new Conta(50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+contaPoupanca.sacar(10);
 
 console.log(contaPoupanca)
 console.log(conta1)
-//console.log(conta2.numeroDeContas)
